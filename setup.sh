02a192e97888dea833519e083157d86f1198ae11
Matt Thompson <matt@echobind.com>
2021-01-07 15:07:50 -0500

    testing 56 w/ config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ source .env
 # File Permissions
 chmod -R a+x ./initializers/
 
-sh ./initializers/mysql57.sh
-sh ./initializers/mysql8.sh
-sh ./initializers/postgres.sh
+sh ./initializers/mysql56.sh
+# sh ./initializers/mysql57.sh
+# sh ./initializers/mysql8.sh
+# sh ./initializers/postgres.sh
